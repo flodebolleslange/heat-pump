b113624e2b200ac16d76543d0dae336ffacc09c3
SystemFailures <aw2032@cam.ac.uk>
2026-06-08 20:53:58 +0100

    Restored the half hourly data for the last year and added simulating ground temperature variations across the year to SPF.py

--- SPF.py
+++ SPF.py
@@ -16,7 +16,7 @@ def getQfloor(Tground):
     U_specific = 40.32
     thickness = 30
     U_total = U_specific/thickness
-    return (Thouse-Tground)*U_total/(house['length']*house['width'])
+    return (Thouse-Tground)*U_total*(house['length']*house['width'])
 
 def getQwalls(Tamb):
     Rbrick = (1/846.67)*102.5 #brick thickness of 102.5mm
@@ -25,34 +25,39 @@ def getQwalls(Tamb):
     Q = (Thouse-Tamb)*house['height']*house['length']/Rtot + (Thouse-Tamb)*house['height']*house['width']*2/Rtot
     return Q
 
-def getQtot(Tamb):
+def getQtot(Tamb,numHalfHours):
+    Tground = getTground(numHalfHours)
     Qroof = getQroof(Tamb)
     Qwalls = getQwalls(Tamb)
-    Qfloor = getQfloor(15)
+    Qfloor = getQfloor(Tground)
     Qtot = Qroof + Qwalls + Qfloor
     return Qtot
 
+def getTground(numHalfHours):
+    x=np.arange(numHalfHours)
+    Tground =  12-7*np.cos(((x+4*30*48)*2*np.pi)/(12*30*48))
+    return Tground
 
 def getTambs():
     Tambs = []
     times = []
-    file = list(open('weather-raw.csv',mode='r'))
+    file = list(open('half-hourly-Tamb.csv',mode='r'))
     file = file[1:]
     for line in file:
         line = line.split(",")
         Tambs.append(float(line[1].rstrip('\n'))/10)
         times.append(line[0][-5:])
 
-    #print(times)
     
-    return Tambs
+    return Tambs,times
 
-def peterFunc(Tambs):
+def peterFunc(Tambs,times):
+    Tthermostat = 17
     # Heat pump assumptions
     radiator = 60.0             # degC
     pinch_point = 5        # K
     isentropic_efficiency = 0.9
-    days = np.arange(1, len(Tambs) + 1)
+    index = np.arange(1, len(Tambs) + 1)
 
 
     # Refrigerant cycle
@@ -74,13 +79,18 @@ def peterFunc(Tambs):
     q_out = H2 - H3
     w_comp = H2 - H1
 
-    COP = q_out / w_comp
+    fanPower = 0.8*245 * 0 #0.8A draw at 245V
+    COP = q_out / (w_comp + fanPower)
 
-    Q_house = getQtot(Tambs)
-    heating = Q_house > 0
+    Q_house = getQtot(Tambs,len(times))
+    #heating = Q_house > 0
+    heating = Tambs < Tthermostat
+    heating2 = np.array([False if int(item[:2])<=6 or int(item[:2])>=22 else True for item in times])
+    heating = np.array(heating) & heating2
 
 
     # compressor power needed to meet demand
+   
     W_required = np.zeros_like(Q_house)
     W_required[heating] = Q_house[heating] / COP[heating]
 
@@ -88,14 +98,20 @@ def peterFunc(Tambs):
     print("SPF =",SPF)
 
 
-    SPF_plot = np.full_like(days, SPF, dtype=float)
+    SPF_plot = np.full_like(index, SPF, dtype=float)
 
     COP_carnot = (Tcond + 273.15) / ((Tcond + 273.15) - (Tevap + 273.15))
 
+    index = index/48
+    Tground = getTground(len(index))
     plt.figure()
-    plt.plot(days, COP, label='Cycle COP')
-    plt.plot(days, SPF_plot, label=f'SPF = {SPF:.2f}')
-    plt.plot(days, COP_carnot, label='Carnot COP')
+    plt.plot(index, COP, label='Cycle COP')
+    plt.plot(index, SPF_plot, label=f'SPF = {SPF:.2f}')
+    plt.plot(index, COP_carnot, label='Carnot COP')
+    plt.plot(index,Tground,label="Ground temperature")
+
+    print("Total work (kWhr)=",np.sum(W_required[heating])/1000*0.5)
+    print("Total heat demand (kWhr)=",np.sum(Q_house[heating])/1000*0.5)
 
 
     plt.xlabel('Day')
@@ -104,20 +120,23 @@ def peterFunc(Tambs):
     plt.legend()
     plt.show()
 
-    plt.plot(days, Q_house, label='Heat Demand (W)')
-    plt.plot(days, W_required, label='Compressor Power Required (W)')
+    plt.plot(index, Q_house, label='Heat Demand (W)')
+    plt.plot(index, W_required, label='Compressor Power Required (W)')
     plt.xlabel('Day')
     plt.ylabel('Power (W)')
     plt.grid(True)
     plt.legend()
     plt.show()
 
-peterFunc(np.array(getTambs()))
-
 def main():
-    Tambs = getTambs()
-    for temp in Tambs:
-        if temp>17:
-            Q = getQtot(temp)
-            #COPe = getCOPe()
-            #W = Q/COPe
+    Tambs, times = getTambs()
+    peterFunc(np.array(Tambs),np.array(times))
+
+main()
+
+def otherMain():
+    qair = 1005 * 5
+    print("q_air =",qair)
+
+    h1 = CP.PropsSI('H','T',15+273)
+    #qrefrig = h1-h4
